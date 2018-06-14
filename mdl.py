c611b163ded1a80585f5b5f614d2bf15e84f5b2e
Karina Ionkina <kionkina@stuy.edu>
2018-06-13 21:01:18 -0400

    WE MADE IT

--- mdl.py
+++ mdl.py
@@ -238,23 +238,6 @@ def p_command_square_pyramid(p):
     cmd['args'] = p[arg_start:arg_start+5]
     commands.append(cmd)
 
-def p_command_square_pyramid(p):
-    """command : SQUARE_PYRAMID NUMBER NUMBER NUMBER NUMBER NUMBER
-               | SQUARE_PYRAMID SYMBOL NUMBER NUMBER NUMBER NUMBER NUMBER
-               | SQUARE_PYRAMID NUMBER NUMBER NUMBER NUMBER NUMBER SYMBOL
-               | SQUARE_PYRAMID SYMBOL NUMBER NUMBER NUMBER NUMBER NUMBER SYMBOL"""
-    cmd = {'op': p[1], 'constants' : None, 'cs' : None, 'args':[]}
-    arg_start = 2
-    if isinstance(p[2],str):
-        cmd['constants'] = p[2]
-        arg_start = 3
-    if len(p) == 8 and isinstance(p[7],str):
-        cmd['cs'] = p[7]
-    if len(p) == 9 and isinstance(p[8],str):
-        cmd['cs'] = p[8]
-    cmd['args'] = p[arg_start:arg_start+5]
-    commands.append(cmd)
-
 def p_command_crystal(p):
     """command : CRYSTAL NUMBER NUMBER NUMBER NUMBER NUMBER
                | CRYSTAL SYMBOL NUMBER NUMBER NUMBER NUMBER NUMBER
@@ -271,6 +254,24 @@ def p_command_crystal(p):
           cmd['cs'] = p[8]
     cmd['args'] = p[arg_start:arg_start+5]
     commands.append(cmd)
+
+def p_command_cylinder(p):
+    """command : CYLINDER NUMBER NUMBER NUMBER NUMBER NUMBER
+               | CYLINDER SYMBOL NUMBER NUMBER NUMBER NUMBER NUMBER
+               | CYLINDER NUMBER NUMBER NUMBER NUMBER NUMBER SYMBOL
+               | CYLINDER SYMBOL NUMBER NUMBER NUMBER NUMBER NUMBER SYMBOL"""
+    cmd = {'op' : p[1], 'constants' : None, 'cs' : None, 'args':[]}
+    arg_start = 2
+    if isinstance(p[2], str):
+        cmd['constants'] = p[2]
+        arg_start = 3
+    if len(p) == 8 and isinstance(p[7], str):
+        cmd['cs'] = p[7]
+    if len(p) == 9 and isinstance(p[8], str):
+          cmd['cs'] = p[8]
+    cmd['args'] = p[arg_start:arg_start+5]
+    commands.append(cmd)
+
 
 def p_command_cone(p):
     """command : CONE NUMBER NUMBER NUMBER NUMBER NUMBER
